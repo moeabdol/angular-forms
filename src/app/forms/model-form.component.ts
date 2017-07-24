19ef08f581a1c66a9a52781a319fc61d90b7df29
moeabdol <mohd.a.saed@gmail.com>
2017-07-24 14:44:48 +0300

    Model driven form validation

--- src/app/forms/model-form.component.ts
+++ src/app/forms/model-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "model-form",
@@ -12,17 +12,41 @@ export class ModelFormComponent implements OnInit {
     "French",
     "German"
   ];
-  myForm: FormGroup;
+  myForm:     FormGroup;
+  firstName:  FormControl;
+  lastName:   FormControl;
+  email:      FormControl;
+  password:   FormControl;
+  language:   FormControl;
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.firstName = new FormControl("", Validators.required);
+    this.lastName = new FormControl("", Validators.required);
+    this.email = new FormControl("", [
+      Validators.required,
+      Validators.pattern("[^ @]*@[^ @]*")
+    ]);
+    this.password = new FormControl("", [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+    this.language = new FormControl();
+  }
+
+  createForm() {
     this.myForm = new FormGroup({
       name: new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl()
+        firstName:  this.firstName,
+        lastName:   this.lastName
       }),
-      email: new FormControl(),
-      password: new FormControl(),
-      language: new FormControl()
+      email:     this.email,
+      password:  this.password,
+      language:  this.language
     });
   }
 }
